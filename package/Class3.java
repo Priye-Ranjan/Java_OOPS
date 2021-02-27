27f22f4a2bcb6ac77279dbc6360ff9ad67874396
Priya Ranjan <50163478+Priye-Ranjan@users.noreply.github.com>
2021-02-27 21:45:17 +0530

    Add files via upload

---
+++ package/Class3.java
@@ -0,0 +1,8 @@
+package Third;
+//import Second.*;
+public class Class3 {
+    public static void main(String[] args) {
+        Second.Class2 obj=new Second.Class2(); //using fully qualified name
+        obj.add(10,30);
+    }
+}
